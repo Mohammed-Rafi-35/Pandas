3b8f113cd2b97d31bd1bd6048e9acdaff0fdad43
Santhosh. S <2005.santhoshsivakumar@gmail.com>
2024-05-22 17:14:14 +0530

    Initial commit

--- Tutorial 2/Tutorial_2.py
+++ Tutorial 2/Tutorial_2.py
@@ -7,3 +7,27 @@ def import_check( DATA ):
         return 'Correct'
     else:
         return 'Incorrect'
+    
+def TASK_1( DATA_1 ):
+    CHECK = pd.read_csv("./Data Sets/YouTube.csv")
+
+    if CHECK.equals( DATA_1 ):
+        return 'Correct'
+    else:
+        return 'Incorrect'
+    
+def TASK_2( DATA_2 ):
+    CHECK = pd.read_csv("./Data Sets/YouTube.csv")
+
+    if DATA_2.equals( CHECK.head(10) ):
+        return 'Correct'
+    else:
+        return 'Incorrect'
+    
+def TASK_3( DATA_3 ):
+    CHECK = pd.read_csv("./Data Sets/YouTube.csv")
+
+    if DATA_3.equals( CHECK.tail(20) ):
+        return 'Correct'
+    else:
+        return 'Incorrect'
